fb3fb9e0ccc49ea66990e035c0ec776ab26f49f4
Resnard <resnardl@gmail.com>
2023-04-28 15:41:53 +0800

    back navigation and profile edits

--- frontend/src/components/cart/OrderSuccess.js
+++ frontend/src/components/cart/OrderSuccess.js
@@ -8,7 +8,7 @@ const OrderSuccess = () => {
     return (
         <Fragment>
             <MetaData title={'Order Success'} />
-            <div className="row justify-content-center">
+            <div className="row justify-content-center white ">
                 <div className="col-6 mt-5 text-center">
 
                     <img className="my-5 img-fluid d-block mx-auto" src="/images/order_success.png" alt="Order Success" width="200" height="200" />
